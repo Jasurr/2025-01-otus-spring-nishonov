772fd12d4e0090f33c389ae5dbd8fa2f2df5d6f9
jasur <njasur.1995@gmail.com>
2025-02-25 00:05:19 +0500

    all tasks without unit test

--- hw03/src/main/java/ru/otus/hw/dao/CsvQuestionDao.java
+++ hw03/src/main/java/ru/otus/hw/dao/CsvQuestionDao.java
@@ -1,11 +1,17 @@
 package ru.otus.hw.dao;
 
+import com.opencsv.bean.CsvToBeanBuilder;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 import ru.otus.hw.config.TestFileNameProvider;
+import ru.otus.hw.dao.dto.QuestionDto;
 import ru.otus.hw.domain.Question;
+import ru.otus.hw.exceptions.QuestionReadException;
 
-import java.util.ArrayList;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.Reader;
 import java.util.List;
 
 @RequiredArgsConstructor
@@ -19,7 +25,24 @@ public class CsvQuestionDao implements QuestionDao {
         // https://opencsv.sourceforge.net/#collection_based_bean_fields_one_to_many_mappings
         // Использовать QuestionReadException
         // Про ресурсы: https://mkyong.com/java/java-read-a-file-from-resources-folder/
-
-        return new ArrayList<>();
+        InputStream inputStream = getClass().getClassLoader().getResourceAsStream(fileNameProvider.getTestFileName());
+        if (inputStream == null) {
+            throw new QuestionReadException("File not found: " + fileNameProvider.getTestFileName());
+        }
+        List<Question> questions;
+        try (Reader reader = new InputStreamReader(inputStream)) {
+            questions = new CsvToBeanBuilder<QuestionDto>(reader)
+                    .withType(QuestionDto.class)
+                    .withSeparator(';')
+                    .withSkipLines(1)
+                    .build()
+                    .parse()
+                    .stream()
+                    .map(QuestionDto::toDomainObject)
+                    .toList();
+        } catch (IOException e) {
+            throw new QuestionReadException("Error reading CSV file", e);
+        }
+        return questions;
     }
 }
--- hw03/src/main/java/ru/otus/hw/service/TestServiceImpl.java
+++ hw03/src/main/java/ru/otus/hw/service/TestServiceImpl.java
@@ -3,8 +3,15 @@ package ru.otus.hw.service;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import ru.otus.hw.dao.QuestionDao;
+import ru.otus.hw.domain.Answer;
+import ru.otus.hw.domain.Question;
 import ru.otus.hw.domain.Student;
 import ru.otus.hw.domain.TestResult;
+import ru.otus.hw.exceptions.QuestionReadException;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
@@ -19,15 +26,55 @@ public class TestServiceImpl implements TestService {
         ioService.printLine("");
         ioService.printLineLocalized("TestService.answer.the.questions");
         ioService.printLine("");
-
-        var questions = questionDao.findAll();
+        List<Question> questions;
+        try {
+            questions = questionDao.findAll();
+        } catch (QuestionReadException e) {
+            String errorMessage = getErrorMessage(e);
+            ioService.printLine(errorMessage);
+            return new TestResult(student);
+        }
         var testResult = new TestResult(student);
 
-        for (var question: questions) {
-            var isAnswerValid = false; // Задать вопрос, получить ответ
+        processQuestions(questions, testResult);
+        return testResult;
+    }
+
+    private void processQuestions(List<Question> questions, TestResult testResult) {
+        for (Question question : questions) {
+            ioService.printLine(question.text());
+            String correctAnswer = displayAnswersAndGetCorrectAnswer(question.answers());
+            String userAnswer = ioService.readString();
+
+            boolean isAnswerValid = userAnswer.equalsIgnoreCase(correctAnswer);
             testResult.applyAnswer(question, isAnswerValid);
         }
-        return testResult;
+    }
+
+    private String displayAnswersAndGetCorrectAnswer(List<Answer> answers) {
+        StringBuilder correctAnswer = new StringBuilder();
+
+        for (int i = 0; i < answers.size(); i++) {
+            char letter = (char) ('A' + i); // A, B, C...
+            var answer = answers.get(i);
+            ioService.printLine(letter + ") " + answer.text());
+
+            if (answer.isCorrect()) {
+                correctAnswer.append(letter);
+            }
+        }
+        return correctAnswer.toString();
+    }
+
+    private String getErrorMessage(QuestionReadException e) {
+        Throwable cause = e.getCause();
+        if (cause instanceof FileNotFoundException) {
+            return "Error: The test file is missing. Please contact support.";
+        } else if (cause instanceof IOException) {
+            return "Error: Unable to read the test file. Please try again later.";
+        } else {
+            return "Error: An unexpected error occurred. Please contact support.";
+        }
     }
 
 }
